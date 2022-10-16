21f8d00cded67f8e39d1413bcff833431853ee28
Fariasmlk <matheusfaria77@uni9.edu.br>
2022-10-16 18:33:32 -0300

    feature/campo-cadastro

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia-component';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+})
+export class AppModule { }
